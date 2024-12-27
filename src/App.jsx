3140916f519824bc1de90a6b20e43ab71f55d746
riyadhbd2 <riyadhbd2@gmail.com>
2024-12-27 23:33:13 +0100

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
       <h1>Welcome to tasker</h1>
+      <Header></Header>
     </div>
   );
 };
